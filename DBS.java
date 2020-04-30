cfaefe05bf70d60672aeec4c903ebc3c15eccb77
Siddhi Burse <siddhirus@gmail.com>
2020-04-30 17:54:26 +0530

    added 3nf to bcnf fn

--- DBS.java
+++ DBS.java
@@ -21,12 +21,12 @@ class Relations{
         for (String s : fd_set) {											
         	for(int i=0; i<s.length(); i++){
         		char[] sch = s.toCharArray();
-        		if (sch[i]=='>'){
+        		if (s.charAt(i)=='>'){
         			if(i+2 != s.length()){
         				for(int j=1; j<=(s.length()-i-1); j++){
         					String decomp = "";
         					decomp = decomp.copyValueOf(sch,0,i+1);
-        					decomp = decomp + sch[i+j];
+        					decomp = decomp + s.charAt(i+j);
         					fd_set1.add(decomp);
         				}
        					storeIndex.add(fd_set.indexOf(s));
@@ -43,22 +43,20 @@ class Relations{
 
        	for(String s: fd_set){						              //removing redundant fd 
        		char[] sch = s.toCharArray();
-       		if((sch[0]==sch[sch.length-1])&&(sch[1]=='-')){
+       		if((sch[0]==s.charAt(s.length()-1))&&(s.charAt(1)=='-')){
        			fd_set.remove(s);
        		}
        	}
 
        	int num_of_attr = (relation.length()-1)/2;				  //finding essential elements of the key
        	HashMap<Character, Integer> map = new HashMap<>();        //initializing map to 0 for all attr
-       	char[] temp = relation.toCharArray();
        	for(int j=1; j<relation.length(); j=j+2){
-       		map.put(temp[j],0);
+       		map.put(relation.charAt(j),0);
        	}
 
        	for(String s: fd_set){                                    //marking all right side attributes
-       		char[] temp1 = s.toCharArray();
        		for (Map.Entry<Character, Integer> entry : map.entrySet()){
-       			if(entry.getKey()==temp1[s.length()-1]){
+       			if(entry.getKey()==s.charAt(s.length()-1)){
        				map.put(entry.getKey(),1);
        			}
        		}	
@@ -72,7 +70,7 @@ class Relations{
        	}
 
        	for(int i=1; i<relation.length(); i=i+2){                   //initializing all relation attr to 0
-       		map.put(temp[i],0);   
+       		map.put(relation.charAt(i),0);   
        	}
 		
        	for(Character c: essential_attr){                           //changing value of essential attr of key to 1 in map
@@ -96,12 +94,11 @@ class Relations{
             List<Character> right = new ArrayList<>();
             List<Character> common = new ArrayList<>();
             for(String s: fd_set){
-                char[] sch = s.toCharArray();
-                for(int i=0; sch[i]!='-'; i++){
-                    left.add(sch[i]);
+                for(int i=0; s.charAt(i)!='-'; i++){
+                    left.add(s.charAt(i));
                 }
-                for(int j=s.length()-1; sch[j]!='>'; j--){
-                    right.add(sch[j]);
+                for(int j=s.length()-1; s.charAt(j)!='>'; j--){
+                    right.add(s.charAt(j));
                 }
             }
             for(Character a: left){
@@ -123,7 +120,7 @@ class Relations{
             for(Character a: common){                               //try adding each common element and checking whether the closure is complete or not
                 essential_attr.add(a);
                 for(int i=1; i<relation.length(); i=i+2){                   
-                     map_new.put(temp[i],0);   
+                     map_new.put(relation.charAt(i),0);   
                 }
                 for(Character b: essential_attr){
                     map_new.put(b,1);
@@ -149,15 +146,14 @@ class Relations{
        	for(Character key: map.keySet()){						
        		if(map.get(key)==1){
        			for(String s:fd_set){
-       				char[] sch = s.toCharArray();
-       				if(key==sch[0]){
-       					for(int i=0; sch[i]!='-'; i++){
-       						if(map.get(sch[i])==0){
+       				if(key==s.charAt(0)){
+       					for(int i=0; s.charAt(i)!='-'; i++){
+       						if(map.get(s.charAt(i))==0){
        							flag=0;
        						}	
        					}
                         if(flag==1){
-                            map.put(sch[sch.length-1],1);
+                            map.put(s.charAt(s.length()-1),1);
                         }
        				}
        			}
@@ -176,6 +172,76 @@ class Relations{
         }
         return complete;
     }
+
+    public static void nf3_to_bcnf(Relations r){
+        List<String> fd_set = new ArrayList<>();
+        StringTokenizer st1 = new StringTokenizer(r.fd, ",{}"); 
+        while (st1.hasMoreTokens()) {
+            fd_set.add(st1.nextToken());
+        }
+        Set<Character> r_set = new HashSet<Character>();
+        StringTokenizer st2 = new StringTokenizer(r.relation, ",{}"); 
+        while (st2.hasMoreTokens()) {
+            Character c = (st2.nextToken()).charAt(0);
+            r_set.add(c);
+        }
+
+        List<String> violates_bcnf = new ArrayList<>();
+        String key = r.find_key(r.relation,r.fd);
+        for(int i=0;i<key.length();i++){
+            for(String s: fd_set){
+                for(int j=s.length()-1; s.charAt(j)!='>'; j--){
+                    if(s.charAt(j)==key.charAt(i)){
+                        violates_bcnf.add(s);
+                    }
+                }
+            }
+            
+        }
+
+        for(String s: violates_bcnf){
+            Set<Character> alpha = new HashSet<Character>();
+            Set<Character> beta = new HashSet<Character>();
+            for(int i=0; s.charAt(i)!='-'; i++){
+                alpha.add(s.charAt(i));
+            }                                       //System.out.println(alpha);
+            
+            for(int j=s.length()-1; s.charAt(j)!='>'; j--){
+                beta.add(s.charAt(j));
+            }                                       //System.out.println(beta); 
+
+            System.out.println("The decomposed relations are: ");
+            Set<Character> union = new HashSet<Character>(alpha); 
+            union.addAll(beta);
+            System.out.print("Relation 1: ");
+            System.out.print(union);  System.out.print("\t");
+            System.out.print("The key of this relation is: ");
+            for(Character a: alpha){
+                System.out.print(a);
+            }System.out.println();
+
+            Set<Character> difference = new HashSet<Character>(beta); 
+            difference.removeAll(alpha); 
+            Set<Character> difference1 = new HashSet<Character>(r_set); 
+            difference1.removeAll(difference); 
+            System.out.print("Relation 2: "); 
+            System.out.print(difference1);    System.out.print("\t");
+            Set<Character> key_of_r2 = new HashSet<Character>(); 
+            for(Character a: difference1){
+                for(int i=0; i<key.length(); i++){
+                    for(Character c: alpha){
+                        if(a==key.charAt(i)||(a==c)){
+                            key_of_r2.add(a);
+                        }
+                    }
+                }
+            }
+            System.out.print("The key of this relation is: ");
+            for(Character b: key_of_r2){
+                System.out.print(b);
+            } System.out.println();
+        }
+    }
 }
 
 class DBS{
@@ -189,5 +255,7 @@ class DBS{
 		Relations r1 = new Relations(relation,fd);
 		String key=r1.find_key(relation,fd);
 		System.out.println("The key of the relation is: " + key);
+
+        r1.nf3_to_bcnf(r1);
 	}
 }
