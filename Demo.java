85801ecc48c4bda8892bbfbf0310a81e80e436c7
Siddhi Burse <siddhirus@gmail.com>
2020-05-15 13:12:33 +0530

    variable GD and BFR

--- Demo.java
+++ Demo.java
@@ -13,7 +13,7 @@ class Demo{
 	public static List<String> hash_fn(List<Integer> keys){
 		List<String> hash_val = new ArrayList<>();
 		for(Integer x: keys){
-			int mod = x%10; 
+			int mod = x%16; 
 			String s = Integer.toBinaryString(mod);
 			while(s.length()!=4){
          		s='0'+s;
@@ -25,25 +25,7 @@ class Demo{
     public static String get_last_digits(String hash_val,int num){
         return (hash_val.substring(4-num,4));
     }
-    public static int power(int x, int y){ 
-        int result = x; 
-        for (int i = 1; i < y; i++){
-        result = result * x; 
-        } 
-        return result; 
-    }
-    public static int get_GD(int[] counti){
-    	int[] pow = new int[2];
-    	for(int i=0; i<2; i++){
-    		if(0<=counti[i] && counti[i]<=2) {pow[i]=1;}
-    		else if(3<=counti[i] && counti[i]<=4) {pow[i]=2;}
-    		else if(5<=counti[i] && counti[i]<=8) {pow[i]=3;}
-    		else if(9<=counti[i] && counti[i]<=16) {pow[i]=4;}
-    		else{ System.out.println("Error: space exceeded!");}
-    	}
-		return(Math.max(pow[0],pow[1]));
-    }
-    
+   
     public static int[] get_num(List<String> hash_val,String s1,String s2,int i){
     	int[] arr = new int[2];
     	for(String s: hash_val){
@@ -75,7 +57,7 @@ class Demo{
 		}
 		System.out.println();
     }
-	public static List<String> Calculate(List<Integer> keys,String oper) throws MemoryExceededException{
+	public static List<String> Calculate(List<Integer> keys,String oper,int bfr,int gd) throws MemoryExceededException{
 		List<String> hash_val = hash_fn(keys);
 		int[] counti = new int[2]; 
 		counti[0]=0; counti[1]=0;
@@ -89,129 +71,129 @@ class Demo{
 		List<String> ans = new ArrayList<>();
 		ans.clear();
 
-			if(counti[0]>2){
+			if(counti[0]>bfr){
 				int[] arr1 = get_num(hash_val,"00","10",2);
-				if(arr1[0]>2){
+				if(arr1[0]>bfr){
 					int[] arr2 = get_num(hash_val,"000","100",3);
-					if(arr2[0]>2){
+					if(arr2[0]>bfr){
 						int[] arr3 = get_num(hash_val,"0000","1000",4);
-						if(arr3[0]>2 || arr3[1]>2){
-							if(arr3[0]>2)   throw new MemoryExceededException(keys,"0000");
-							if(arr3[1]>2) 	throw new MemoryExceededException(keys,"1000");
+						if(arr3[0]>bfr || arr3[1]>bfr){
+							if(arr3[0]>bfr)   throw new MemoryExceededException(keys,"0000");
+							if(arr3[1]>bfr) 	throw new MemoryExceededException(keys,"1000");
 						}
 						else{
-							if(arr3[0]<=2) { ans.add("0000");}
-							if( arr3[1]<=2) { ans.add("1000");}
+							if(arr3[0]<=bfr) { ans.add("0000");}
+							if( arr3[1]<=bfr) { ans.add("1000");}
 						}
 					}
-					else if(arr2[0]<=2) { ans.add("000"); }
-					if(arr2[1]>2){
+					else if(arr2[0]<=bfr) { ans.add("000"); }
+					if(arr2[1]>bfr){
 						int[] arr3 = get_num(hash_val,"0100","1100",4);
-						if(arr3[0]>2 || arr3[1]>2){
-							if(arr3[0]>2) throw new MemoryExceededException(keys,"0100");
-							if(arr3[1]>2) throw new MemoryExceededException(keys,"1100");
+						if(arr3[0]>bfr || arr3[1]>bfr){
+							if(arr3[0]>bfr) throw new MemoryExceededException(keys,"0100");
+							if(arr3[1]>bfr) throw new MemoryExceededException(keys,"1100");
 						}
 						else{
-							if( arr3[0]<=2) { ans.add("0100");}
-							if( arr3[1]<=2) { ans.add("1100");}
+							if( arr3[0]<=bfr) { ans.add("0100");}
+							if( arr3[1]<=bfr) { ans.add("1100");}
 						}
 					}
-					else if( arr2[1]<=2) {  ans.add("100"); }
+					else if( arr2[1]<=bfr) {  ans.add("100"); }
 				}
-				else if( arr1[0]<=2) {  ans.add("00"); }
-				if(arr1[1]>2){
+				else if( arr1[0]<=bfr) {  ans.add("00"); }
+				if(arr1[1]>bfr){
 					int[] arr2 = get_num(hash_val,"010","110",3);
-					if(arr2[0]>2){
+					if(arr2[0]>bfr){
 						int[] arr3 = get_num(hash_val,"0010","1010",4);
-						if(arr3[0]>2 || arr3[1]>2){ 
-							if(arr3[0]>2) throw new MemoryExceededException(keys,"0010");
-							if(arr3[1]>2) throw new MemoryExceededException(keys,"1010");
+						if(arr3[0]>bfr || arr3[1]>bfr){ 
+							if(arr3[0]>bfr) throw new MemoryExceededException(keys,"0010");
+							if(arr3[1]>bfr) throw new MemoryExceededException(keys,"1010");
 						}
 						else{
-							if(arr3[0]>0 && arr3[0]<=2) { ans.add("0010");}
-							if(arr3[1]>0 && arr3[1]<=2) { ans.add("1010");}
+							if(arr3[0]>0 && arr3[0]<=bfr) { ans.add("0010");}
+							if(arr3[1]>0 && arr3[1]<=bfr) { ans.add("1010");}
 						}
 					}
-					else if(arr2[0]>0 && arr2[0]<=2) {  ans.add("010"); }
-					if(arr2[1]>2){
+					else if(arr2[0]>0 && arr2[0]<=bfr) {  ans.add("010"); }
+					if(arr2[1]>bfr){
 						int[] arr3 = get_num(hash_val,"0110","1110",4);
-						if(arr3[0]>2 || arr3[1]>2){
-							if(arr3[0]>2) throw new MemoryExceededException(keys,"0110");
-							if(arr3[1]>2) throw new MemoryExceededException(keys,"1110");
+						if(arr3[0]>2 || arr3[1]>bfr){
+							if(arr3[0]>bfr) throw new MemoryExceededException(keys,"0110");
+							if(arr3[1]>bfr) throw new MemoryExceededException(keys,"1110");
 						}
 						else{
-							if(arr3[0]>0 && arr3[0]<=2) { ans.add("0110");}
-							if(arr3[1]>0 && arr3[1]<=2) { ans.add("1110");}
+							if(arr3[0]>0 && arr3[0]<=bfr) { ans.add("0110");}
+							if(arr3[1]>0 && arr3[1]<=bfr) { ans.add("1110");}
 						}
 					}
-					else if(arr2[1]>0 && arr2[1]<=2) {  ans.add("110"); }
+					else if(arr2[1]>0 && arr2[1]<=bfr) {  ans.add("110"); }
 				}
-				else if(arr1[0]>0 && arr1[0]<=2) {  ans.add("10"); }
+				else if(arr1[0]>0 && arr1[0]<=bfr) {  ans.add("10"); }
 				
 			}
-			else if(counti[0]>0 && counti[0]<=2){
+			else if(counti[0]>0 && counti[0]<=bfr){
 				ans.add("0");
 			}
-			if(counti[1]>2){
+			if(counti[1]>bfr){
 				int[] arr1 = get_num(hash_val,"01","11",2);
-				if(arr1[0]>2){
+				if(arr1[0]>bfr){
 					int[] arr2 = get_num(hash_val,"001","101",3);
-					if(arr2[0]>2){
+					if(arr2[0]>bfr){
 						int[] arr3 = get_num(hash_val,"0001","1001",4);
-						if(arr3[0]>2 || arr3[1]>2){
-							if(arr3[0]>2) throw new MemoryExceededException(keys,"0001");
-							if(arr3[1]>2) throw new MemoryExceededException(keys,"1001");
+						if(arr3[0]>bfr || arr3[1]>bfr){
+							if(arr3[0]>bfr) throw new MemoryExceededException(keys,"0001");
+							if(arr3[1]>bfr) throw new MemoryExceededException(keys,"1001");
 						}
 						else{
-							if(arr3[0]>0 && arr3[0]<=2) { ans.add("0001");}
-							if(arr3[1]>0 && arr3[1]<=2) { ans.add("1001");}
+							if(arr3[0]>0 && arr3[0]<=bfr) { ans.add("0001");}
+							if(arr3[1]>0 && arr3[1]<=bfr) { ans.add("1001");}
 						}
 					}
-					else if(arr2[0]>0 && arr2[0]<=2) {  ans.add("001"); }
-					if(arr2[1]>2){
+					else if(arr2[0]>0 && arr2[0]<=bfr) {  ans.add("001"); }
+					if(arr2[1]>bfr){
 						int[] arr3 = get_num(hash_val,"0101","1101",4);
-						if(arr3[0]>2 || arr3[1]>2){
-						 	if(arr3[0]>2) throw new MemoryExceededException(keys,"0101");
-							if(arr3[1]>2) throw new MemoryExceededException(keys,"1101");
+						if(arr3[0]>bfr || arr3[1]>bfr){
+						 	if(arr3[0]>bfr) throw new MemoryExceededException(keys,"0101");
+							if(arr3[1]>bfr) throw new MemoryExceededException(keys,"1101");
 						}
 						else{
-							if(arr3[0]>0 && arr3[0]<=2) { ans.add("0101");}
-							if(arr3[1]>0 && arr3[1]<=2) { ans.add("1101");}
+							if(arr3[0]>0 && arr3[0]<=bfr) { ans.add("0101");}
+							if(arr3[1]>0 && arr3[1]<=bfr) { ans.add("1101");}
 						}
 					}
-					else if(arr2[1]>0 && arr2[1]<=2) {  ans.add("101"); }
+					else if(arr2[1]>0 && arr2[1]<=bfr) {  ans.add("101"); }
 				}
-				else if(arr1[0]>0 && arr1[0]<=2) {  ans.add("01"); }
-				if(arr1[1]>2){
+				else if(arr1[0]>0 && arr1[0]<=bfr) {  ans.add("01"); }
+				if(arr1[1]>bfr){
 					int[] arr2 = get_num(hash_val,"011","111",3);
-					if(arr2[0]>2){
+					if(arr2[0]>bfr){
 						int[] arr3 = get_num(hash_val,"0011","1011",4);
-						if(arr3[0]>2 || arr3[1]>2){
-							if(arr3[0]>2) throw new MemoryExceededException(keys,"0011");
-							if(arr3[1]>2) throw new MemoryExceededException(keys,"1011");
+						if(arr3[0]>bfr || arr3[1]>bfr){
+							if(arr3[0]>bfr) throw new MemoryExceededException(keys,"0011");
+							if(arr3[1]>bfr) throw new MemoryExceededException(keys,"1011");
 						}
 						else{
-							if(arr3[0]>0 && arr3[0]<=2) { ans.add("0011");}
-							if(arr3[1]>0 && arr3[1]<=2) { ans.add("1011");}
+							if(arr3[0]>0 && arr3[0]<=bfr) { ans.add("0011");}
+							if(arr3[1]>0 && arr3[1]<=bfr) { ans.add("1011");}
 						}
 					}
-					else if(arr2[0]>0 && arr2[0]<=2) {  ans.add("011"); }
-					if(arr2[1]>2){
+					else if(arr2[0]>0 && arr2[0]<=bfr) {  ans.add("011"); }
+					if(arr2[1]>bfr){
 						int[] arr3 = get_num(hash_val,"0111","1111",4);
-						if(arr3[0]>2 || arr3[1]>2){
-							if(arr3[0]>2) throw new MemoryExceededException(keys,"0111"); 
-							if(arr3[1]>2) throw new MemoryExceededException(keys,"1111"); 
+						if(arr3[0]>bfr || arr3[1]>bfr){
+							if(arr3[0]>bfr) throw new MemoryExceededException(keys,"0111"); 
+							if(arr3[1]>bfr) throw new MemoryExceededException(keys,"1111"); 
 						}
 						else{
-							if(arr3[0]>0 && arr3[0]<=2) { ans.add("0111");}
-							if(arr3[1]>0 && arr3[1]<=2) { ans.add("1111");}
+							if(arr3[0]>0 && arr3[0]<=bfr) { ans.add("0111");}
+							if(arr3[1]>0 && arr3[1]<=bfr) { ans.add("1111");}
 						}
 					}
-					else if(arr2[1]>0 && arr2[1]<=2) {  ans.add("111"); }
+					else if(arr2[1]>0 && arr2[1]<=bfr) {  ans.add("111"); }
 				}
-				else if(arr1[1]>0 && arr1[1]<=2) {  ans.add("11"); }
+				else if(arr1[1]>0 && arr1[1]<=bfr) {  ans.add("11"); }
 			}
-			else if(counti[1]>0 && counti[1]<=2){
+			else if(counti[1]>0 && counti[1]<=bfr){
 				ans.add("1");
 			}
 
@@ -226,11 +208,17 @@ class Demo{
 				GD=s.length();
 			}
 		}
-
+		GD = max(GD,gd);
 		if(oper.equals("insert")||oper.equals("delete")){
 			print_result(ans,array,GD);
 		}
 		return ans;
+	}
+	public static int max(int x,int y){
+		if(x>y)
+			return x;
+		else 
+			return y;
 	}
 	public static void print_result(List<String> ans,int[][] array,int GD){
 		System.out.println("GD: "+GD);
@@ -249,7 +237,7 @@ class Demo{
 			System.out.println();
 		}
 	}
-	public static void search(List<Integer> keys,int x){
+	public static void search(List<Integer> keys,int x,int bfr,int gd){
 		int mod = x%10; 
 		String s = Integer.toBinaryString(mod);
 		while(s.length()!=4){
@@ -257,7 +245,7 @@ class Demo{
         }
 		List<String> buckets = new ArrayList<>();
 		try{
-			buckets = Calculate(keys,"search");
+			buckets = Calculate(keys,"search",bfr,gd);
 		}
 		catch(MemoryExceededException mee) { return;}
 
@@ -265,16 +253,22 @@ class Demo{
 			if(s1.equals(get_last_digits(s,s1.length()))){
 				System.out.println("Key "+x+" is present in bucket "+s1);
 			}
-			
 		}
 		
 	}
 	public static void main(String args[]){
+		System.out.print("Enter the GD: ");
+		Scanner sc = new Scanner(System.in);
+		String input1 = sc.nextLine();
+        int gd = Integer.parseInt(input1);	System.out.println(gd);
+        System.out.print("Enter the BFR: ");
+        String input2 = sc.nextLine();
+        int bfr = Integer.parseInt(input2);	System.out.println(bfr);
 		System.out.print("Enter the number of operations: ");
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         int num = Integer.parseInt(input);
-        System.out.println(num);
+        //System.out.println(num);
         List<Integer> store = new ArrayList<>();
         
         while(num!=0){
@@ -290,7 +284,7 @@ class Demo{
                 }
                 else{
                     store.add(key);
-                    try{  Calculate(store,"insert"); }
+                    try{  Calculate(store,"insert",bfr,gd); }
                     catch(MemoryExceededException mee){
                     	return;
                     }
@@ -302,7 +296,7 @@ class Demo{
                 int key = Integer.parseInt(key_string);  
                 if(store.contains(key)){
                     store.remove(new Integer(key));
-                    try{  Calculate(store,"delete"); }
+                    try{  Calculate(store,"delete",bfr,gd); }
                     catch(MemoryExceededException mee){
                     	return;
                     } 
@@ -316,7 +310,7 @@ class Demo{
             	String key_string = sc.nextLine();
             	int key = Integer.parseInt(key_string);
             	if(store.contains(key)){
-            		search(store,key);
+            		search(store,key,bfr,gd);
             	}
             	else{
             		System.out.println("Error: Missing key error!\n\tThe entered key does not exist in the database!");
